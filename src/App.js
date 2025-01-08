2776a5a50daea47a5909eb36e0215836e563e657
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2025-01-08 14:23:37 +0700

    onClickButton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React,{Component} from 'react'
-import Calendar from './class-components';
-import { Content, Footer, Header } from './func-components';
-
+import Button from './class-components';
 
 function App() {
-  return (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  );
+  return <Button></Button>
 }
 
 export default App;
